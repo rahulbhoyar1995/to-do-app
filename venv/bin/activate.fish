2653252d63f3d4b8644828cb1f5da772a4ef3d62
Rahul Bhoyar <rahulbhoyraoffice@gmail.com>
2023-07-17 17:16:47 +0530

    Initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/rahulbhoyar/Desktop/Rahul_PC/Gitlab_Projects/Todo_App/venv"
+set -gx VIRTUAL_ENV "/Users/rahulbhoyar/Desktop/Rahul_PC/Gitlab_Projects/to-do-app-using-django/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
